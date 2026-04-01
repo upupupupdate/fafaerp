d05a9095e013411cd53793e8e3711983ecb149ac
unknown <77077305@qq.com>
2026-04-01 15:22:30 +0800

    fix: 修复发布路径404问题

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/fafaerp/',
   plugins: [vue()],
   resolve: {
     alias: {
