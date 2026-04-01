ca104c438bad19b89762dc667ee31aba143bd447
unknown <77077305@qq.com>
2026-04-01 15:50:56 +0800

    fix: 适配 Vercel 根目录路径

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/fafaerp/',
+  base: './',
   plugins: [vue()],
   resolve: {
     alias: {
